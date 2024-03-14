852b416e5d25972eb96605d856d647f717bea9db
Kittypoda <odakrolstad@gmail.com>
2024-03-14 11:49:05 +0100

    do fetch martin

---
+++ script/index.mjs
@@ -0,0 +1,7 @@
+import { allFilms } from "./fetchData.mjs";
+import { displayJackets } from "./display-jackets.mjs";
+import { filterFunctions } from "./filter-by-genre.mjs";
+
+
+displayJackets(allJackets);
+filterFunctions();
