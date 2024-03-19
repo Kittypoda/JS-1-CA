446f56309f8d0fdc648ba7aec2911bfa461103a7
Kittypoda <odakrolstad@gmail.com>
2024-03-19 14:15:28 +0100

    products on index + checkout setup

--- script/index.mjs
+++ script/index.mjs
@@ -1,7 +1,67 @@
-import { allFilms } from "./fetchData.mjs";
-import { displayJackets } from "./display-jackets.mjs";
-import { filterFunctions } from "./filter-by-genre.mjs";
+import { API_RAIN_URL } from "./constants.mjs";
+import { doFetch } from "./utilitys/doFetch.mjs";
+
+function generateJacketHtml(jacket) {
+  const jacketWrapper = document.createElement('div');
+  jacketWrapper.classList.add('jacket-wrapper');
+
+  const jacketContainer = document.createElement('div');
+  jacketContainer.classList.add('jacket-container');
+
+  const heading = document.createElement('h3');
+  heading.textContent = jacket.title;
+
+  const jacketPriceContainer = document.createElement('div');
+
+  const jacketImageContainer = document.createElement('img');
+  jacketImageContainer.src = jacket.image.url;
+  jacketImageContainer.alt = jacket.image.alt;
+  
+
+  const jacketPrice = document.createElement('div');
+  jacketPrice.textContent = jacket.price;
+
+  const jacketDiscountedPrice = document.createElement('div');
+  jacketDiscountedPrice.textContent = jacket.discountedPrice;
+
+  const jacketBuyButton = document.createElement('button');
+  jacketBuyButton.textContent = 'Buy';
+  jacketBuyButton.classList.add('jacket-buy-button');
+  jacketBuyButton.addEventListener('click', () => {
+    addToCart(jacket);
+  });
 
 
-displayJackets(allJackets);
-filterFunctions();
+  jacketPriceContainer.append(jacketPrice, jacketDiscountedPrice);
+  jacketContainer.append(jacketImageContainer,heading, jacketPriceContainer, jacketBuyButton);
+  jacketWrapper.appendChild(jacketContainer);
+
+  return jacketWrapper;
+}
+
+function displayJackets(jackets) {
+  const jacketsDisplayContainer = document.getElementById('jackets-display');
+  console.log(jacketsDisplayContainer);
+  jacketsDisplayContainer.textContent = '';
+  jackets
+  
+    .forEach((jacket) => {
+      const jacketHtml = generateJacketHtml(jacket);
+      jacketsDisplayContainer.appendChild(jacketHtml);
+    });
+}
+
+async function renderHomePage() {
+  const responseData = await doFetch(API_RAIN_URL);
+  const jackets = responseData.data;
+  console.log(jackets);
+  displayJackets(jackets);
+}
+
+function main() {
+  renderHomePage()
+  const jackets = doFetch(API_RAIN_URL);
+  
+}
+
+main();
